c7a1482d516be3ba883586e714e7c7bc447cc08e
Bastian <Bastian@T400>
2017-03-25 14:00:28 +0100

    added euler integrator

--- src/simulate_pendulum.cc
+++ src/simulate_pendulum.cc
@@ -8,6 +8,8 @@
 #include "double_pendulum.h"
 #include "integrator.h"
 #include "rungekutta4.h"
+#include "euler.h"
+
 
 
 int main(int argc, char *argv[])
@@ -15,31 +17,53 @@ int main(int argc, char *argv[])
     std::vector<std::string> args(argv, argv+argc);
 
     DynamicalSystem* system = nullptr;
-	RungeKutta4<DynamicalSystem,
-		std::vector<double>> integrator = {0.1};
+    Integrator<DynamicalSystem, std::vector<double>>* integrator = nullptr;
 
-    if ((args.size() > 1) && (args[1] == "single")) {
+    bool error = false;
 
-        system = new Pendulum();
+    if ((args.size() > 2) && (args[1] == "single")) {
 
-    } else if ((args.size() > 1) && (args[1] == "double")) {
+      system = new Pendulum();
 
-        system = new DoublePendulum();
+    }
+    else if ((args.size() > 2) && (args[1] == "double")) {
+
+      system = new DoublePendulum();
 
     } else {
 
-      std::cout << "Usage: program [single | double] \n";
-      std::cout << "for single or double pendulum. \n";
+      error = true;
+
+    }
+
+
+    if ((error == false) && (args.size() > 2) && (args[2] == "euler")) {
+
+      integrator = new Euler<DynamicalSystem, std::vector<double>>(0.1);
+
+    } else if ((error == false) && (args.size() > 2) && (args[2] == "rk4")) {
+
+      integrator = new RungeKutta4<DynamicalSystem, std::vector<double>>(0.1);
+
+    } else {
+      error == true;
+    }
+
+    if (error == true) {
+      std::cout << "Usage: program [single | double] [euler | rk4] \n";
+      std::cout << "[single | double ]for single or double pendulum. \n";
+      std::cout << "[euler | rk4] for Euler or Runge Kutta4 scheme. \n";
 
       return 1;
 
     }
 
-    auto data = integrator.integrateNSteps(*system, system->getInitialState(),
+    auto data = integrator->integrateNSteps(*system, system->getInitialState(),
         system->getInitialTime(), 100);
     integration::writeIntegrationData(data);
 
     delete system;
+    delete integrator;
 
     return 0;
 
