2575dfac3d439f496d73c98142562ff68f5cb981
bn2302 <bastian.niebel@gmail.com>
2017-03-25 14:53:19 +0100

    removed debug folder

--- debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bastian/workspace/dynamical_systems/test
-# Build directory: /home/bastian/workspace/dynamical_systems/debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TARGETS "test_dynamical_system")
-subdirs(../googletest-build)
