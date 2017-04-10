83757cc25b73e47c8b99d09c8d1b33515c449e08
bn2302 <bastian.niebel@gmail.com>
2017-04-10 23:44:50 +0200

    cleaning up

--- src/simulate_pendulum.cc
+++ src/simulate_pendulum.cc
@@ -1,45 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <memory>
 
 #include "dynamical_system.h"
 #include "pendulum.h"
 #include "double_pendulum.h"
 #include "integrator.h"
 #include "rungekutta4.h"
+#include "euler.h"
+
 
 
 int main(int argc, char *argv[])
 {
     std::vector<std::string> args(argv, argv+argc);
 
-    DynamicalSystem* system = nullptr;
-	RungeKutta4<DynamicalSystem,
-		std::vector<double>> integrator = {0.1};
+    std::unique_ptr<DynamicalSystem> system;
+    std::unique_ptr<Integrator<DynamicalSystem, std::vector<double>>> 
+        integrator;
 
-    if ((args.size() > 1) && (args[1] == "single")) {
+    bool error = false;
 
-        system = new Pendulum();
+    if ((args.size() > 2) && (args[1] == "single")) {
 
-    } else if ((args.size() > 1) && (args[1] == "double")) {
+      system = std::make_unique<Pendulum>();
 
-        system = new DoublePendulum();
+    }
+    else if ((args.size() > 2) && (args[1] == "double")) {
+
+      system = std::make_unique<DoublePendulum>();
 
     } else {
 
-      std::cout << "Usage: program [single | double] \n";
-      std::cout << "for single or double pendulum. \n";
+      error = true;
+
+    }
+
+
+    if ((error == false) && (args.size() > 2) && (args[2] == "euler")) {
+
+      integrator = std::make_unique<
+          Euler<DynamicalSystem, std::vector<double>>>(0.1);
+
+    } else if ((error == false) && (args.size() > 2) && (args[2] == "rk4")) {
+
+      integrator = std::make_unique<
+          RungeKutta4<DynamicalSystem, std::vector<double>>>(0.1);
+
+    } else {
+      error = true;
+    }
+
+    if (error == true) {
+      std::cout << "Usage: program [single | double] [euler | rk4] \n";
+      std::cout << "[single | double ]for single or double pendulum. \n";
+      std::cout << "[euler | rk4] for Euler or Runge Kutta4 scheme. \n";
 
       return 1;
 
     }
 
-    auto data = integrator.integrateNSteps(*system, system->getInitialState(),
+    auto data = integrator->integrateNSteps(*system, system->getInitialState(),
         system->getInitialTime(), 100);
     integration::writeIntegrationData(data);
-
-    delete system;
 
     return 0;
 
